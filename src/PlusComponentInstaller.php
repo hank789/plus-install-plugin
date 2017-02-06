c1fbc4727617a59d6438ad0a3aa60f5d7778396c
Seven <shiweidu@outlook.com>
2017-02-06 14:17:10 +0800

    add test.

--- src/PlusComponentInstaller.php
+++ src/PlusComponentInstaller.php
@@ -6,6 +6,8 @@ use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 use Composer\Repository\InstalledRepositoryInterface;
 use InvalidArgumentException;
+use Composer\DependencyResolver\Pool;
+use Composer\Package\Package;
 
 class PlusComponentInstaller extends LibraryInstaller
 {
@@ -22,6 +24,50 @@ class PlusComponentInstaller extends LibraryInstaller
         $isPlusComponent = $packageType === 'plus-component';
 
         return $isPlusComponent;
+    }
+
+    /**
+     * Resolves a package link to a package in the installed pool
+     *
+     * Since dependencies are already installed this should always find one.
+     *
+     * @param Pool $pool Pool of installed packages only
+     * @param Link $link Package link to look up
+     *
+     * @return PackageInterface|null The found package
+     */
+    private function lookupInstalledPackage(Pool $pool, Link $link)
+    {
+        $packages = $pool->whatProvides($link->getTarget(), $link->getConstraint());
+
+        return (!empty($packages)) ? $packages[0] : null;
+    }
+
+    /**
+     * Recursively generates a map of package names to packages for all deps
+     *
+     * @param Pool             $pool      Package pool of installed packages
+     * @param array            $collected Current state of the map for recursion
+     * @param PackageInterface $package   The package to analyze
+     *
+     * @return array Map of package names to packages
+     */
+    private function collectDependencies(Pool $pool, array $collected, PackageInterface $package)
+    {
+        $requires = array_merge(
+            $package->getRequires(),
+            $package->getDevRequires()
+        );
+
+        foreach ($requires as $requireLink) {
+            $requiredPackage = $this->lookupInstalledPackage($pool, $requireLink);
+            if ($requiredPackage && !isset($collected[$requiredPackage->getName()])) {
+                $collected[$requiredPackage->getName()] = $requiredPackage;
+                $collected = $this->collectDependencies($pool, $collected, $requiredPackage);
+            }
+        }
+
+        return $collected;
     }
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
@@ -46,13 +92,23 @@ class PlusComponentInstaller extends LibraryInstaller
         // run installer.
         parent::install($repo, $package);
 
-        $generator = $this->composer->getAutoloadGenerator();
-        $map = $package->getAutoload();
+        $localRepo = $this->composer->getRepositoryManager()->getLocalRepository();
+        $pool = new Pool('dev');
+        $pool->addRepository($localRepo);
 
+        $autoloadPackages = array($package->getName() => $package);
+        $autoloadPackages = $this->collectDependencies($pool, $autoloadPackages, $package);
+
+        $generator = $this->composer->getAutoloadGenerator();
+        $autoloads = array();
+        foreach ($autoloadPackages as $autoloadPackage) {
+            $downloadPath = $this->getInstallPath($autoloadPackage, (null && $globalRepo->hasPackage($autoloadPackage)));
+            $autoloads[] = array($autoloadPackage, $downloadPath);
+        }
+
+        $map = $generator->parseAutoloads($autoloads, new Package('dummy', '1.0.0.0', '1.0.0'));
         $classLoader = $generator->createLoader($map);
         $classLoader->register();
-
-        var_dump($classLoader);exit;
 
         $installerClass = $extra['installer-class'];
 
