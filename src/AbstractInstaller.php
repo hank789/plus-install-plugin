2be674eef63db25055e04d598fb8e76d08bc3879
Seven <shiweidu@outlook.com>
2017-02-08 17:52:28 +0800

    完成更多接口和抽象

---
+++ src/AbstractInstaller.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Zhiyi\Component\Installer\PlusInstallPlugin;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
+
+abstract class AbstractInstaller implements InstallerInterface
+{
+    /**
+     * The component:install commmand instance.
+     */
+    protected function $command;
+
+    /**
+     * The command output instance.
+     */
+    protected function $output;
+
+    /**
+     * 构造方法
+     *
+     * @param Illuminate\Console\Command $command
+     * @param Illuminate\Console\OutputStyle $output
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    protected function __construct(Command $command, OutputStyle $output)
+    {
+        $this->command = $command;
+        $this->output = $output;
+    }
+
+    /**
+     * 路由配置.
+     *
+     * @return string 路由配置文件列表
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function router()
+    {
+    }
+
+    /**
+     * 静态资源.
+     *
+     * @return array 静态资源文件列表
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function resource()
+    {
+    }
+
+    /**
+     * 应用安装.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function install()
+    {
+        $this->output->success("Installed the {$this->getName()}");
+    }
+
+    /**
+     * 应用升级.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function update()
+    {
+        $this->output->success("Updated The {$this->getName()}");
+    }
+
+    /**
+     * 应用卸载.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function uninstall()
+    {
+        $this->output->success("Uninstall The {$this->getName()}");
+    }
+}
--- src/InstallerInterface.php
+++ src/InstallerInterface.php
@@ -2,8 +2,72 @@
 
 namespace Zhiyi\Component\Installer\PlusInstallPlugin;
 
+use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
+
 interface InstallerInterface
 {
+    /**
+     * 构造方法.
+     *
+     * @param Illuminate\Console\Command $command
+     * @param Illuminate\Console\OutputStyle $output
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function __construct(Command $command, OutputStyle $output);
+
+    /**
+     * Get app name.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function getName(): string;
+
+    /**
+     * get app version.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function getVersion(): string;
+
+    /**
+     * get app logo URL.
+     *
+     * @return string URL
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function getLogo(): string;
+
+    /**
+     * Get developer info.
+     *
+     * @return array
+     * @see FQSEN
+     * ```php
+     * 
+     * public function getAuthor(): array
+     * {
+     *     return [
+     *         'name' => 'Seven Du',
+     *         'email' => 'shiweidu@outlook.com',
+     *         'homepage' => 'http://medz.cn'
+     *     ];
+     * }
+     * 
+     * ```
+     * 
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function getAuthor(): array
+
     /**
      * 应用安装.
      *
